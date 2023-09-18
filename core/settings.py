4893eaf419c406c3ee9808ce582a15834b6e57bb
Chandresh2996 <chandreshsharma@techoma.io>
2023-09-18 17:19:51 +0530

    updated settings.py

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,10 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# AWS
+BASE_DIR =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 import mimetypes
 
@@ -120,27 +123,27 @@ if False:
     }
 else:
     #AWS Testing database
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'databaseNeti',
-            'USER': 'Netisuperuser',
-            'PASSWORD': 'Neti1234',
-            'HOST': 'database-1.cwkh99l3smwi.us-east-1.rds.amazonaws.com',
-            'PORT': '5432',
-        }
-    }
-    #Main Database.
     # DATABASES = {
     #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #         'NAME': 'netidb',
-    #         'USER': 'netitest',
-    #         'PASSWORD': 'netidb@123',
-    #         'HOST': 'localhost',
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': 'databaseNeti',
+    #         'USER': 'Netisuperuser',
+    #         'PASSWORD': 'Neti1234',
+    #         'HOST': 'database-1.cwkh99l3smwi.us-east-1.rds.amazonaws.com',
     #         'PORT': '5432',
     #     }
     # }
+    # Main Database.
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'netidb',
+            'USER': 'netitest',
+            'PASSWORD': 'netidb@123',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -176,11 +179,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR/'static',]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
